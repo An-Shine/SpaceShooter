f00e1d321b6527123ef4eb7ab0582fd27aa127fd
An-Shine <akh1012@naver.com>
2025-08-27 14:32:52 +0900

    serialized

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    Transform tr;
-
+    [SerializeField]    Transform tr;
+    [SerializeField]    float moveSpeed = 10.0f;
     void Start()
     {
         tr = GetComponent<Transform>();
@@ -19,6 +20,7 @@ public class PlayerCtrl : MonoBehaviour
         //transform.position += new Vector3(0, 0, 1);
 
         //transform.position += Vector3.forward * 1;
-        tr.position += Vector3.forward * 1;
+        //tr.position += Vector3.forward * 1;
+        tr.Translate(Vector3.forward * Time.deltaTime*moveSpeed,Space.Self);
     }
 }
