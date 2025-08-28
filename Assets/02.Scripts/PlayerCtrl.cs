ecef23e81631cba6f96f2219acff5a4dfa484092
An-Shine <akh1012@naver.com>
2025-08-28 15:44:29 +0900

    dd

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -7,6 +7,7 @@ public class PlayerCtrl : MonoBehaviour
 {
     [SerializeField]    Transform tr;
     [SerializeField]    float moveSpeed = 10.0f;
+    [SerializeField]    float turnSpeed = 80.0f;
     void Start()
     {
         tr = GetComponent<Transform>();
@@ -15,12 +16,16 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
         Debug.Log($"h={h}");
         Debug.Log($"v={v}");
         //transform.position += new Vector3(0, 0, 1);
 
         //transform.position += Vector3.forward * 1;
         //tr.position += Vector3.forward * 1;
-        tr.Translate(Vector3.forward * Time.deltaTime*moveSpeed,Space.Self);
+        //tr.Translate(Vector3.forward * Time.deltaTime*moveSpeed,Space.Self);
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDir * moveSpeed * Time.deltaTime);
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
