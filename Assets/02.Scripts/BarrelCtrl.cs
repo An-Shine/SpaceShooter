278e12a9ff7c9687cf735df3b2c72022a4e91978
An-Shine <akh1012@naver.com>
2025-09-11 15:49:44 +0900

    dd

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class BarrelCtrl : MonoBehaviour
 {
     public GameObject expEffect;
+    public Texture[] textures;
+    public float radius = 10.0f;
+    new MeshRenderer renderer;
     Transform tr;
     Rigidbody rb;
     int hitCount = 0;
+    Collider[] colls = new Collider[10];
 
     const int HIT_COUNT = 3;
     const float DESTROY_EXP = 5.0f;
     const float DESTROY_BARREL = 3.0f;
     const float BARREL_MASS = 1.0f;
     const float UP_FORCE = 1500.0f;
+    
+
 
 
 
@@ -22,9 +29,16 @@ public class BarrelCtrl : MonoBehaviour
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
+        renderer = GetComponentInChildren<MeshRenderer>();
+
+        int idx = Random.Range(0, textures.Length);     // 난수 발생
+
+        renderer.material.mainTexture = textures[idx];  //텍스쳐 지정
+
+
     }
 
-    void OCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Bullet"))
         {
@@ -38,10 +52,28 @@ public class BarrelCtrl : MonoBehaviour
     {
         GameObject exp = Instantiate(expEffect, tr.position, Quaternion.identity);
         Destroy(exp, DESTROY_EXP);
-        rb.mass = BARREL_MASS;
-        rb.AddForce(Vector3.up * UP_FORCE);
+        //rb.mass = BARREL_MASS;
+        //rb.AddForce(Vector3.up * UP_FORCE);
+        IndirectDamage(tr.position);
+
         Destroy(gameObject, DESTROY_BARREL);
 
 
     }
+
+    void IndirectDamage(Vector3 pos)
+    {
+        //Collider[] colls = Physics.OverlapSphere(pos, radius, 1 << 3);
+        Physics.OverlapSphereNonAlloc(pos, radius, colls, 1 << 3);
+        foreach (var item in colls)
+        {
+            if (item == null) continue;
+            rb = item.GetComponent<Rigidbody>();
+            rb.mass = 1.0f;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddExplosionForce(1500.0f, pos, radius, 1200.0f);
+        }
+    }
+
+
 }
--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     // Start is called before the first frame update
     void OnCollisionEnter(Collision collision)
     {
@@ -12,6 +13,12 @@ public class RemoveBullet : MonoBehaviour
         //if(collision.gameObject.tag == "Bulet")
         if(collision.collider.CompareTag("Bullet"))
         {
+            ContactPoint cp = collision.GetContact(0);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+            //Instantiate(sparkEffect, cp.point, rot);
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+            Destroy(spark, 0.5f);
+
             Destroy(collision.gameObject);
         }
     }
