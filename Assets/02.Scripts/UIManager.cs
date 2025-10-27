2814defcc77fcb2f190cf075513a6989870ec926
An-Shine <akh1012@naver.com>
2025-10-27 14:02:05 +0900

    dd

--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 public class UIManager : MonoBehaviour
-{
+{    
     public void OnButtonClick(RectTransform rt)    
     {
         Debug.Log($"Click Button : {rt.localScale.x}");
     }
-}    
+}
+/*
+shopButton.onClick.AddListener(
+    () => OnButtonClick(shopButton.name));
+
+void Start()    
+{
+    action = () -> OnStartClick();    
+}
+
+public void OnButtonClick(string msg)
+{
+    Debug.Log($"Clilck Button : {msg}");
+}
+
+
+public void OnButtonClick()
+{
+    SceneManager.LoadScene("Level_01");
+    SceneManager.LoadScene("Play", LoadSceneMode.Additive);
+}
+*/
